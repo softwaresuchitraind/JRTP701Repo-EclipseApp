5a9d2a0ff501e24135d4474e199bd2e40faf8388
softwaresuchitraind <software.suchitra@gmail.com>
2025-01-28 21:28:27 +0530

    Commited java

--- MavenProj01/src/main/java/com/sukla/Main/App.java
+++ MavenProj01/src/main/java/com/sukla/Main/App.java
@@ -8,6 +8,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("DEV code");
+        System.out.println("Hello World!");
+        System.out.println("DEV codeSKS");
         System.out.println("DEV code123");
     }
 
